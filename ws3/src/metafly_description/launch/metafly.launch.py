a48afa08fe434bd5d5e5fc91200538441fefeac2
Phillipp <pgery@purdue.edu>
2025-10-19 12:54:22 -0400

    finfisl final push ;D

--- ws3/src/metafly_description/launch/metafly.launch.py
+++ ws3/src/metafly_description/launch/metafly.launch.py
@@ -62,13 +62,19 @@ def generate_launch_description():
     
     # fix the joints at a specific position for all time
     # using a new feature of the joint_state_publisher
+    # joint_state_publisher_node = Node(
+    #     package="joint_state_publisher",
+    #     executable="joint_state_publisher",
+    #     name="joint_state_publisher",
+    #     output="both",
+    #     parameters=[robot_description, {'zeros.joint1': 0.707, 'zeros.joint2': -0.707}],
+    # )
+
     joint_state_publisher_node = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        name="joint_state_publisher",
-        output="both",
-        parameters=[robot_description, {'zeros.joint1': 0.707, 'zeros.joint2': -0.707}],
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
     )
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
